c991ec7c9699c9c1d72541b64d2e496b2bf7dbd2
CaroCap <carolinecap.event@gmail.com>
2022-02-22 13:01:54 +0100

    DAL Services BienEchange

---
+++ HolidayRental.Common/Repositories/IAvisMembreBienRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolidayRental.Common.Repositories
+{
+    public interface IAvisMembreBienRepository<TAvisMembreBien> : IRepository<TAvisMembreBien, int>
+    {
+        
+    }
+}
---
+++ HolidayRental.DAL/Repositories/AvisMembreBienService.cs
@@ -0,0 +1,47 @@
+﻿using HolidayRental.Common.Repositories;
+using HolidayRental.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace HolidayRental.DAL.Repositories
+{
+    public class AvisMembreBienService : ServiceBase, IAvisMembreBienRepository<AvisMembreBienDAL>
+    {
+        public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_connString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "DELETE FROM [AvisMembreBien] WHERE [idAvis] = @id";
+                    SqlParameter p_id = new SqlParameter() { ParameterName = "id", Value = id };
+                    command.Parameters.Add(p_id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public AvisMembreBienDAL Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AvisMembreBienDAL> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(AvisMembreBienDAL entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, AvisMembreBienDAL entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
